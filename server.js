c98859c156825e1272e167f92943c8c060461d20
Hillary Fan <hillary.fan123@gmail.com>
2020-01-07 14:06:48 -0800

    integrating to heroku postgres

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const knex = require('knex');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-dimensional-61752',
-    user : 'hifan',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
