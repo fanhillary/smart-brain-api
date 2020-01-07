c0f4eeec4b8be407ab20f0c139849906ed5dfc5a
Hillary Fan <hillary.fan123@gmail.com>
2020-01-06 16:38:48 -0800

    adding dynamic port

--- server.js
+++ server.js
@@ -96,6 +96,6 @@ app.put('/image', (req,res) => {
     .catch(err => res.status(404).json('error updating entries'))
 })
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
 })
